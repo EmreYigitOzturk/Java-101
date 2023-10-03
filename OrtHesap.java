5fa59a2fe821fd6d10a8581a8fa8324feb77f46e
emre <dogala90@gmail.com>
2023-10-03 16:10:38 +0300

    program tamamlandi

---
+++ OrtHesap.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class OrtHesap {
+    public static void main(String[] args) {
+        int mat,fizik,kimya,turkce,tarih,muzik;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("matematik notunuzu giriniz : ");
+        mat = input.nextInt();
+        System.out.println("fizik notunuzu giriniz : ");
+        fizik = input.nextInt();
+        System.out.println("kimya notunuzu giriniz : ");
+        kimya = input.nextInt();
+        System.out.println("turkce notunuzu giriniz : ");
+        turkce = input.nextInt();
+        System.out.println("tarih notunuzu giriniz : ");
+        tarih = input.nextInt();
+        System.out.println("muzik notunuzu giriniz : ");
+        muzik = input.nextInt();
+
+        double sonuc = (mat+fizik+kimya+turkce+tarih+muzik)/6;
+        System.out.println("Ortalamanız : "+sonuc);
+
+
+    }
+}
